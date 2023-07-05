8185b7c316e3a7211b2d196477a74d9bed5cf186
Martin Muriuki <helheim254@gmail.com>
2023-07-05 10:52:43 +0300

    Display locations bases=d on destination query

--- main.js
+++ main.js
@@ -16,5 +16,22 @@ function getRecommendations() {
         },
       };
     
+      // Make an API request using fetch
+      fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          // Clear previous recommendations
+          destinationList.innerHTML = "";
+    
+          // Display locations
+          data.data.forEach(location => {
+            const listItem = document.createElement("li");
+            listItem.textContent = location.result_object.name;
+            destinationList.appendChild(listItem);
+          });
+        })
+        .catch(error => {
+          console.error("Error fetching locations:", error);
+        });
     }
   
