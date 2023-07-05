1b17a0000fbba1481baa92ddcf2ffe1cf1c20300
Martin Muriuki <helheim254@gmail.com>
2023-07-05 10:36:30 +0300

    Get user input from form

---
+++ main.js
@@ -0,0 +1,12 @@
+function getRecommendations() {
+    const destinationInput = document.getElementById("destination");
+    const budgetInput = document.getElementById("budget");
+    const durationInput = document.getElementById("duration");
+    const destinationList = document.getElementById("destinationList");
+  
+    const destination = destinationInput.value;
+    const budget = budgetInput.value;
+    const duration = durationInput.value;
+  
+}
+  
