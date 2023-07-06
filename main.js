a063a7ffbf9f18cb7c163c168432030532a25312
Martin Muriuki <helheim254@gmail.com>
2023-07-06 13:25:28 +0300

    Style the API response

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+document.addEventListener('DOMContentLoaded', function () {
+    getRecommendations();
+})
+
 function getRecommendations() {
     const destinationInput = document.getElementById("destination");
     const budgetInput = document.getElementById("budget");
@@ -8,17 +11,18 @@ function getRecommendations() {
     const destination = destinationInput.value;
     const budget = budgetInput.value;
     const duration = durationInput.value;
+    const limit = 10;
 
     const requestOptions = {
         method: "GET",
         headers: {
-            "x-rapidapi-key": process.env,
+            "x-rapidapi-key": '',
             "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
         },
     };
 
     // Make an API request using fetch
-    fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}`, requestOptions)
+    fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}&limit=${limit}`, requestOptions)
         .then(response => response.json())
         .then(data => {
             // Clear previous recommendations
@@ -29,12 +33,18 @@ function getRecommendations() {
                 const listItem = document.createElement("li");
                 // const listImage = document.createElement('img');
                 listItem.textContent = location.result_object.name;
+                listItem.style.padding = "10px";
+                listItem.style.border = "1px solid #ccc";
+                listItem.style.borderRadius = "5px";
+                listItem.style.marginBottom = "10px";
+                
                 const photoRef = location.result_object.photo ? location.result_object.photo.images.original.url : null;
-
                 if (photoRef) {
                     // Create image element and set source attribute
                     const image = document.createElement("img");
                     image.src = photoRef;
+                    image.style.maxWidth = "50%";
+                    image.style.height = "auto";
                     listItem.appendChild(image);
                 }
                 destinationList.appendChild(listItem);
