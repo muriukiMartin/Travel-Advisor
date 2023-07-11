145d885f822ca106980ea24284d5db0901d90d73
Martin Muriuki <helheim254@gmail.com>
2023-07-11 16:01:06 +0300

    Add folders

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     getRecommendations();
+// })
+
+// function getRecommendations() {
+//     const destinationInput = document.getElementById("destination");
+//     const destinationList = document.getElementById("destinationList");
+
+//     const destination = destinationInput.value;
+//     const limit = 10;
+//     const requestOptions = {
+//         method: "GET",
+//         headers: {
+//           "x-rapidapi-key": "",
+//           "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+//         },
+//       };
+    
+//       const hotelsEndpoint = `https://travel-advisor.p.rapidapi.com/locations/search?query=${destination}&limit=${limit}`;
+
+//   fetch(hotelsEndpoint, requestOptions)
+//     .then(response => response.json())
+//     .then(data => {
+//       const hotels = data.data.filter(item => item.result_type === "lodging");
+//       destinationList.innerHTML = ""; // Clear previous recommendations
+
+//       if (hotels.length > 0) {
+//         hotels.forEach(hotel => {
+//           const hotelItem = createHotelItem(hotel.result_object.name, hotel.result_object.rating);
+//           destinationList.appendChild(hotelItem);
+//         });
+//       } else {
+//         const noHotelsItem = createNoDataItem("No hotels available");
+//         destinationList.appendChild(noHotelsItem);
+//       }
+//     })
+//     .catch(error => {
+//       console.error("Error fetching hotel data:", error);
+//     });
+// }
+    
+//     function createHotelItem(name, rating) {
+//       const item = document.createElement("div");
+//       item.className = "hotel-item";
+    
+//       const nameElement = document.createElement("h3");
+//       nameElement.textContent = name;
+    
+//       const ratingElement = document.createElement("p");
+//       ratingElement.textContent = `Rating: ${rating}`;
+    
+//       item.appendChild(nameElement);
+//       item.appendChild(ratingElement);
+    
+//       return item;
+//     }
+    
+//     function createNoDataItem(message) {
+//       const item = document.createElement("div");
+//       item.className = "no-data-item";
+    
+//       const messageElement = document.createElement("p");
+//       messageElement.textContent = message;
+    
+//       item.appendChild(messageElement);
+    
+//       return item;
+//     }
